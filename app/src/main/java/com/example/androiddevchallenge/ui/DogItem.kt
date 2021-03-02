cebfab88e84912e6fc0896281b584c4f84f20b46
xxia2-sh <xxia2@stubhub.com>
2021-03-02 17:24:54 +0800

    fix lint

--- app/src/main/java/com/example/androiddevchallenge/ui/DogItem.kt
+++ app/src/main/java/com/example/androiddevchallenge/ui/DogItem.kt
@@ -15,7 +15,6 @@
  */
 package com.example.androiddevchallenge.ui
 
-import androidx.compose.foundation.Image
 import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
@@ -31,11 +30,9 @@ import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.layout.ContentScale
-import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.Dp
 import androidx.compose.ui.unit.dp
-import com.example.androiddevchallenge.R
 import com.example.androiddevchallenge.model.Dog
 import com.example.androiddevchallenge.model.dogs
 import com.example.androiddevchallenge.ui.theme.MyTheme
